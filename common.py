bc813fbc5e18cf787b5bf45a7035240ad984d866
weidi.jin <weidi.jin@qunar.com>
2019-12-20 11:57:37 +0800

    add common class file: Buffer class implemented

---
+++ common.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+class Buffer(object):
+
+    def __init__(self, bytes_data):
+        if bytes_data:
+            self.__data = bytes_data[0:]
+            self.__len = len(self.__data)
+        else:
+            self.__data = None
+            self.__len = 0
+        self.__idx = 0
+
+    def pop(self, nbytes): 
+        if (not self.__data) or self.__idx >= self.__len or nbytes < 1:
+            return None
+        rt = self.__data[self.__idx:self.__idx+nbytes]
+        self.__idx += len(rt)
+        return rt
+
+    def is_empty(self):
+        if (not self.bytes_data) or self.__idx >= self.__len:
+            return True
+        return False
